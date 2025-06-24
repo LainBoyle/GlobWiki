3942a6c5b403ccf351153fb4eafb548fa5bd7299
LainBoyle <lainsboyle@gmail.com>
2025-06-24 13:49:18 -0700

    descriptions

--- main.js
+++ main.js
@@ -3,43 +3,46 @@ const globs = [
     name: 'Glob',
     image: 'assets/glob.png',
     rarity: 'Common',
-    description: 'The classic Glob.'
+    description: `What can we say about Globs? You’ve seen ‘em. Globs might go to the store, but they don’t do things like: thinking about the things that they’re not doing. Some of them sometimes have jobs, others don’t know what that is. You know ’em. My uncle (married in) was a Glob. He was a chill guy. He actually decided to stop being a Glob, though. Not because anything really bad happened, just because he felt like that period of his life was over. That’s just glob stuff. Right? All of this is to say: Globs are chillers.`
   },
   {
     name: 'Blue Hat Glob',
     image: 'assets/bluehatglob.png',
     rarity: 'Uncommon',
-    description: 'A Glob sporting a snazzy blue hat.'
+    description: `Andrew Saxon is a very interesting individual. He is stylish and very upset that the game has reduced him to the meager title of “Blue Hat Glob.” His name is Andrew Saxon. Say it with me: Andrew Saxon. I don’t wanna talk about it anymore.
+Andrew Saxon enjoys hiking in scenic locations and stargazing. He’s the guy who always brings a snack with him (in his hat). Nobody ever asks about why his hat makes his head disappear, but it does.`
   },
   {
     name: 'Green Pants Glob',
     image: 'assets/greenpantsglob.png',
     rarity: 'Uncommon',
-    description: 'Known for its bright green pants.'
+    description: `Green Pants Glob lives on some paintings and napkins as a drawing. He is on the back of some cardboard, and not in your yard. You might catch him hiding behind a tree. Don’t look at him, he’s shy. He’s a groovy fellow. He enjoys potted plants and fancy pants, avoids passing glances, and molasses, uh, crasses. Woohoo. Voted Most Likely to be Run Into at the Supermarket in Glob school.`
   },
   {
     name: 'Split Dye Bob Glob',
     image: 'assets/splitdyebobglob.png',
     rarity: 'Rare',
-    description: 'A Glob with stylish split dye hair.'
+    description: `Her name is short for Split-Dye Roberta Glob. Her friends call her SD Berty G. Can be seen at the local umbrella store, and also enjoys cantaloupe. There is no cantaloupe store. Loves sidewalk chalk and hiking, and she also hands people glasses of water, and walks past plants. She likes to think of herself as "Pretty Split-Dye Bob Glob."`
   },
   {
     name: 'Cute Split Dye Bob Glob',
     image: 'assets/cutesplitdyebobglob.png',
     rarity: 'Rare',
-    description: 'Adorable version of split dye bob.'
+    description: `Split-Dye Bob Glob variant. 
+Her name is also short for Split-Dye Roberta Glob. Kind of a Gloth girl - rocks her style with an iron fist. Despite always being mistaken for SD Berty G, she actually has a completely unique set of interests and talents. For example: She can tie her necktie into an Eldredge Knot while riding a horse. And she's a real girls' girl. You know, ride or tie.`
   },
   {
     name: 'Evil Glob',
     image: 'assets/evilglob.png',
     rarity: 'Epic',
-    description: 'Watch out for this one.'
+    description: `Not much is known about them. We actually have no evidence that they're evil at all. They have a clean criminal record and keep to themself for the most part. I think they just call him Evil Glob because his eyes look like that. I don't really know him that well, to be honest.`
   },
   {
     name: 'Midas',
     image: 'assets/midas.png',
     rarity: 'Legendary',
-    description: 'Everything it touches turns to gold.'
+    description: `This Glob’s painted smile hides a dark reality for this Glob. Anything he touches turns to Glob! I mean Golb! I mean, you know. Gourd. Whatever. Midas can’t be bought for the worlb. I mean-jeez. Anywho, he is sad because of that, since he cannot hold his family's hands, or hold hands with old ladies at the crosswalk, or any other similarly noble Glob pursuits. However because he’s a Glob, nothing that bad can actually really happen to him, so it’s all ok.
+He likes to wear expensive fabrics. His favorite foods are golden lemon meringue pie, golden lemons, golden soup, and more. He has a metal water bottle, and drinks metal water. He has a heart of gold. Has a very inefficient bicycle, and an equally inefficient car. His favorite color is green.`
   }
 ];
 
